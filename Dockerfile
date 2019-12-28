8d864b84391ce7ff22e6c18763be1aff8f348035
Hannes Dröse <hd@HDs-MacBook-Pro.local>
2019-12-28 15:54:23 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM mcr.microsoft.com/mssql/server:2017-latest
+ 
+ENV ACCEPT_EULA=Y
+ENV SA_PASSWORD=1234abCD
+ENV MSSQL_PID=Developer
+ENV MSSQL_TCP_PORT=1433
+
+WORKDIR /src
+COPY ./src/ .
+
+RUN (/opt/mssql/bin/sqlservr & ) | grep -q "Service Broker manager has started" && \
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i restore.sql
+
+EXPOSE 1433
---
+++ src/restore.sql
@@ -0,0 +1,16 @@
+USE master;
+
+GO -----------------------------------------------------------------------------
+
+/*
+-- Files need to be moved to the correct location!
+-- use this to see which files are needed
+RESTORE FILELISTONLY
+FROM DISK = '/src/data/SW1920.bak'
+-- */
+
+RESTORE DATABASE sw
+FROM DISK = '/src/SW1920.bak'
+WITH
+	MOVE 'SW1920_Data' TO '/src/SW1920_Data'
+,	MOVE 'SW1920_Log'  TO '/src/SW1920_Log'
