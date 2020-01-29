a5a72dad193c41082d75ea1bce6d72cb97665e11
Hannes Dröse <hd@HDs-MacBook-Pro.local>
2020-01-29 17:02:56 +0100

    Switched to v3

--- src/restore.sql
+++ src/restore.sql
@@ -10,7 +10,7 @@ FROM DISK = '/src/data/SW1920.bak'
 -- */
 
 RESTORE DATABASE sw
-FROM DISK = '/src/SW1920_V2.bak'
+FROM DISK = '/src/SW1920_V3.bak'
 WITH
 	MOVE 'SW1920_Data' TO '/src/SW1920_Data'
 ,	MOVE 'SW1920_Log'  TO '/src/SW1920_Log'
